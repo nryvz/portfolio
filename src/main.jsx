5007a2b521e0a1bd99529f369b5f5008d8cc1b89
Nuriye Yavuz <109990478+nryvz@users.noreply.github.com>
2025-03-23 16:23:39 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
+<StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
 
