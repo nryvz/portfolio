8b4f7ad6e1de9c53f197edd6d65bf4ee4016f420
Nuriye Yavuz <109990478+nryvz@users.noreply.github.com>
2025-03-28 17:21:47 +0300

    index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,14 @@
 import close from "./close.svg";
 import frontend from "./front-end.png";
-import github from "./github.png";
+import github from "./github.svg";
 import nocode from "./laptop-code.png";
 import menu from "./menu.svg";
 import logo from "./ny-logo.svg";
-import react from "./science.png";
+import react from "./science.svg";
 
 import css from "./tech/css.png";
 import figma from "./tech/figma.png";
-import framer from "./tech/framer.png";
+import framer from "./tech/framer.svg";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -24,8 +24,8 @@ import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
 import cg from "./cg.svg";
-import note_al from "./note_al.jpg";
-import weather from "./weather1.jpg";
+import note_al from "./note_al.svg";
+import weather from "./weather1.svg";
 
 export {
   cg,
